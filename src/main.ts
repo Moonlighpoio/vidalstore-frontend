91fca63f2fffe6cda57e7ae7413472fdaf9bf9d3
Renato <ren.barriga@duocuc.cl@duocuc.cl>
2026-09-15 23:17:51 -0300

    feat: configure Amplify with Cognito PKCE

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { Amplify } from 'aws-amplify';
 import { App } from './app/app';
+import { appConfig } from './app/app.config';
+import { amplifyConfig } from './app/core/auth/amplify.config';
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+// Inicialización previa obligatoria
+Amplify.configure(amplifyConfig);
+
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
