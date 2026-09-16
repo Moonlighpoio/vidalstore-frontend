06302b5b92c967a84f44ca0afd65afc5ff92d71a
Renato <ren.barriga@duocuc.cl@duocuc.cl>
2026-09-15 21:48:06 -0300

    feature/member-a-auth

---
+++ src/app/core/auth/cognito.config.ts
@@ -0,0 +1,19 @@
+import { environment } from '../../../environments/environment';
+
+export const cognitoAuthConfig = {
+  Auth: {
+    Cognito: {
+      userPoolId: environment.cognito.userPoolId,
+      userPoolClientId: environment.cognito.appClientId,
+      loginWith: {
+        oauth: {
+          domain: environment.cognito.domain.replace('https://', ''),
+          scopes: environment.cognito.scopes,
+          redirectSignIn: [environment.cognito.redirectSignIn],
+          redirectSignOut: [environment.cognito.redirectSignOut],
+          responseType: 'code' as const
+        }
+      }
+    }
+  }
+};
